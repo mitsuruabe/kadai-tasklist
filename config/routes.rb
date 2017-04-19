13df222de649c78c6fc42183cb400584ca3139c2
mitsuruabe <abem0146@gmail.com>
2017-04-19 01:51:34 +0000

    add signup kadai-tasklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  
   root to: 'toppages#index'
+   
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
   
   resources :tasks
 end
