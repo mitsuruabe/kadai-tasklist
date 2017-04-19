a9460ff403642766e12686b1935d0743695668d2
mitsuruabe <abem0146@gmail.com>
2017-04-19 02:37:23 +0000

    add loing Auth.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
    
   get 'users/index'
   get 'users/show'
